de6003b9db8d1cd965c88d739e713b55c0312103
Rizky Ardiansyah <126098455+RizkyArdiansyahhh@users.noreply.github.com>
2026-07-27 08:13:38 +0700

    feat: add hello header to LoopiPage component

--- src/features/chatbot/pages/loopi-page.tsx
+++ src/features/chatbot/pages/loopi-page.tsx
@@ -1,7 +1,12 @@
 import LoopiChat from "@/features/chatbot/components/loopi-chat";
 
 const LoopiPage = () => {
-  return <LoopiChat />;
+  return (
+    <>
+      <h1>hello</h1>
+      <LoopiChat />
+    </>
+  );
 };
 
 export default LoopiPage;
